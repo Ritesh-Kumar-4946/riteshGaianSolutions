ac8d8a8f0455bc6f1d454fc462f89fc8762e7f81
Riteshkumar.4946 <riteshkumar.4946@gmail.com>
2022-12-08 18:42:38 +0530

    Initial commit

---
+++ app/src/main/java/com/ritesh/gaiansolutions/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.ritesh.gaiansolutions
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.ritesh.gaiansolutions.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+
+    }
+}
